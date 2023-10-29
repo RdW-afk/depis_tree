633371f0430e88a593f228befc344e7c1ab66394
Mr- 0 <oldcrocs21@gmail.com>
2023-10-29 13:43:19 +0000

    X00TD: rewrite device.mk
    Signed-off-by: Mr- 0 <oldcrocs21@gmail.com>

--- afterlife_X00TD.mk
+++ afterlife_X00TD.mk
@@ -9,13 +9,16 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit some common AfterLife stuff
-$(call inherit-product, vendor/afterlife/config/common.mk)
+$(call inherit-product, vendor/afterlife/config/common_full_phone.mk)
 
 # AfterLife flags
-AFTERLIFE_MAINTAINER := Romeo_Delta_Whiskey
+AFTERLIFE_MAINTAINER := Romeo_Delta_Wishkey
 
 # Bootanimation
 TARGET_BOOT_ANIMATION_RES := 1080
+
+# Offline Charging
+USE_PIXEL_CHARGING := true
 
 # disable/enable blur support, default is false
 TARGET_SUPPORTS_BLUR := true
